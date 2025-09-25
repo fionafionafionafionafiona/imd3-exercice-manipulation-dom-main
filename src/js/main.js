ed9aa93d3de605a5435d00dcd733bfeee5e18a22
fionaperina <fiona.perina@studentfr.ch>
2025-09-25 15:15:42 +0200

    exercices 1 et 2 finis

--- src/js/main.js
+++ src/js/main.js
@@ -5,10 +5,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (exercice1) {
     // Ecrire ici tout le code pour l'exercice 1: Sélecteurs JavaScript
+    document.querySelector(".broccoli").remove();
+    document.querySelector("#green-veggie").remove();
+    document.querySelector('[data-vegetable="broccoli"]').remove();
+    document.querySelector(".steamed").remove();
+    document.querySelector(".raw-food").remove();
   }
 
   if (exercice2) {
     // Ecrire ici tout le code pour l'exercice 2: Modification des classes
+    document.querySelector(".raw").classList.add("cooked");
+    document.querySelector(".hot").classList.remove("burned");
+    document.querySelector("#pizza-special").classList.toggle("highlighted");
+    document.querySelector(".basic").classList.add("premium", "deluxe");
+    document.querySelector(".reheated").classList.remove("cold");
+    document.querySelector(".reheated").classList.add("hot");
   }
 
   if (exercice3) {
